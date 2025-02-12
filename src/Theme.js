80405fef3ba31bf0caab6415a12791d820faa6f5
KhushiMhasange <99010742+KhushiMhasange@users.noreply.github.com>
2025-02-12 23:46:27 +0530

    themes

--- src/Theme.js
+++ src/Theme.js
@@ -8,8 +8,8 @@ const getTheme = () =>{
    const theme = localStorage.getItem("theme");
 
    if(!theme){
-     localStorage.setItem("theme","dark");
-     return "dark";
+     localStorage.setItem("theme","light");
+     return "light";
    }
    else return theme;
 };
